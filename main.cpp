06c9517b440d9d4e212bdc175e75f84f546607b7
kevinpecro <kevinpecro@live.fr>
2018-05-14 14:43:31 +0200

    Début du projet

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+matrice::matrice(int pnbl,int pnbc):vecteur2(pnbl*pnbc)
+{
+    nbl=pnbl; nbc=pnbc;
+}
+
+int main()
+{
+
+    int tmp[]={1,2,3,4,5,6};
+
+    matrice m1(2,3); m1.affiche();
+    matrice m2(2,3,tmp);m2.affiche();
+    matrice m3(2,2);m3.affiche();
+
+
+}
